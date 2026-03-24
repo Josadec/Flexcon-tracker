8ea4bbd23ea59cc6b2429b9e25aa3317a583f81c
Josadec Pedraza <josadec@gmail.com>
2026-03-23 21:43:17 -0700

    Remove some files from Larevel

--- storage/framework/views/c1b83f10b3fd4426b51d3300b75d9940.php
+++
@@ -1,2 +0,0 @@
-<img src="<?php echo e(asset('flexcon.png')); ?>" alt="FlexCon" <?php echo e($attributes); ?> />
-<?php /**PATH C:\xampp\htdocs\Flexcon-tracker\resources\views/components/app-logo-icon.blade.php ENDPATH**/ ?>
